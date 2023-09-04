f0d9753232ae7b0a2834cc9d658123e67467f481
KarlBager <kallebager@gmail.com>
2023-09-04 12:46:03 +0200

    color mesh virker

--- script.js
+++ script.js
@@ -16,28 +16,63 @@ function changeColorBack(){
 
 
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  
+}
+
+function setRandomColor() {
+  $("#colorpad").css("background-color", getRandomColor());
+}
+
 
 
 
 document.querySelector('gridEl3');
 document.querySelector('textRandomColor');
 function changeColorRandom(){
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-        
-      }
-      
-      function setRandomColor() {
-        $("#colorpad").css("background-color", getRandomColor());
-      }
     color = getRandomColor();
     console.log(color);
     gridEl3.style.backgroundColor = getRandomColor();
     textRandomColor.style.color = getRandomColor();
+}
+
+
+
+
+let visualGridRow = 1;
+document.querySelector('visualGridContainer');
+
+
+nextRowTrigger = 9;
+nextRowTriggers = [nextRowTrigger];
+for (var j = 0; j < 9; j++){
+nextRowTrigger = nextRowTrigger + 10;
+nextRowTriggers.push(nextRowTrigger);
+}
+console.log(nextRowTriggers);
+
+
+
+for (var i = 0; i < 100; i++){
+
+let createCells = document.createElement('div');
+className = "grid-element visualGrid" + i;
+createCells.setAttribute('class', className);
+createCells.style.gridRow = visualGridRow;
+createCells.style.backgroundColor = getRandomColor();
+
+if(nextRowTriggers.includes(i)){
+visualGridRow++;
+}
+
+console.log(className);
+
+visualGridContainer.appendChild(createCells);
 }
 
